8a9a73d5aa251708df1249cd300489fd2e8ea5c8
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-24 19:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,3 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10)
- 
-    dodger.style.left = `${left - 1}px`
-  }
-})
